9eda85c23dc4ff0c1ea3ec6e312d782da1c43a0b
ZennSky <108292979+ZennSky@users.noreply.github.com>
2022-07-26 20:23:12 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -4,7 +4,7 @@ let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 conn.sendHydrated(m.chat, '*『 𝐈𝐍𝐅𝐎 𝐁𝐎𝐓𝐙 』*', `ʜᴀʟᴏ!, ᴀssᴀʟᴀᴍᴜᴀʟᴀɪᴋᴜᴍ sᴇᴍᴜᴀ😁. ᴋᴇɴᴀʟɪɴ ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ ʏᴀɴɢ ᴅɪ ʙᴇʀɪ ɴᴀᴍᴀ ᴢᴇɴɴsᴋʏ-ʙᴏᴛᴢ ᴏʟᴇʜ ᴏᴡɴᴇʀᴋᴜ<𝟹,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://chat.whatsapp.com/KNkpoT4oX5yFA18GlPttgl', 'ɢʀᴜᴘ ʙᴏᴛᴢ', null, null, [
 [`ᴍᴇɴᴜ`, '.m'],
-[`\nGua sebenernya kang coly ges tapi ya jangan bilang siapa² ya!🤫🤭`, 'Bilek']
+[`\nGua sebenernya kang coly guys🤫🤭`, 'Bilek']
 ], m, {asLocation: true})
 }
 handler.help = ['menu']
